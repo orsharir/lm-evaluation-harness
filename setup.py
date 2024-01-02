dd80db49f374de8f80e96b369e9917336e03981d
Or Sharir <or@sharir.org>
2024-01-02 10:41:06 +0200

    Fix import, again..
    Signed-off-by: Or Sharir <or@sharir.org>

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/EleutherAI/lm-evaluation-harness",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
